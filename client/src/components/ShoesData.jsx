35435c8b4fa8fdd56149a21986e0c100f2d75ad9
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-27 22:58:55 +0300

    Added pagination

--- client/src/components/ShoesData.jsx
+++ client/src/components/ShoesData.jsx
@@ -3,12 +3,14 @@ import { Row, Col, Button } from "react-bootstrap";
 import data from "../data/data.json";
 
 import utilsStyle from "../styles/utils.module.css";
-
 import ShoesList from "./ShoesList";
+
+const productsPerRow = 10;
 
 function ShoesData() {
     const [products, setProducts] = useState([]);
     const [dataLoaded, setDataLoaded] = useState(false);
+    const [next, setNext] = useState(productsPerRow)
 
     useEffect(() => {
         const productsArray = data.shoes;
@@ -16,9 +18,14 @@ function ShoesData() {
         setDataLoaded(true);
     }, []);
 
+   
+    const handleMoreProducts = () => {
+        setNext(next + productsPerRow)
+    }
+
     const productsGrid = (
         <Row xs={1} md={2} xl={3} xxl={4} className="g-4 mt-3 mb-4">
-            {products.map((product) => (
+            {products?.slice(0, next).map((product) => (
                 <Col key={product.id}>
                     <ShoesList product={product} />
                 </Col>
@@ -26,19 +33,19 @@ function ShoesData() {
         </Row>
     );
 
-    const homePage = (
+    const shoesList = (
         <>
             {products.length > 0 ? productsGrid : <h4>No products to show</h4>}
             <div
                 className={`${utilsStyle.blockCenter} ${utilsStyle.flexCenter}`}
             >
-                <Button variant="outline-dark" className="mt-3 mb-5">
+                <Button onClick={handleMoreProducts} variant="outline-dark" className="mt-3 mb-5">
                     Load More
                 </Button>
             </div>
         </>
     );
-    return <>{homePage}</>;
+    return <>{shoesList}</>;
 }
 
 export default ShoesData;
