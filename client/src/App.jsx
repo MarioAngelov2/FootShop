ea9959e45103d5bae655fd2547d5d12a227c93ba
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-28 10:26:16 +0300

    Added filter component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,24 +4,31 @@ import { Route, Routes } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/global.css";
 
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import NavigationBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import DropdownMenu from "./components/DropdownMenu";
 import ShoesData from "./components/ShoesData";
 import BagsData from "./components/BagsData";
+import Filter from "./components/Filter";
+import { SortingProvider } from "./context/DataContext";
 
 function App() {
     return (
         <Container>
             <NavigationBar />
-            <DropdownMenu />
-            <Routes>
-                <Route path="/shoes" element={<ShoesData />} />
-                <Route path="/bags" element={<BagsData />} />
-            </Routes>
-
+            <Row>
+                <Col className="mt-5" md={2}>
+                    <Filter />
+                </Col>
+                <Col md={10}>
+                    <Routes>
+                        <Route path="/shoes" element={<ShoesData />} />
+                        <Route path="/bags" element={<BagsData />} />
+                    </Routes>
+                </Col>
+            </Row>
             <Footer />
         </Container>
     );
