b160fe4a114e445cc1ed9f5aeeb2580d87725e7d
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-28 21:26:25 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,27 +8,28 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import NavigationBar from "./components/Navbar";
 import Footer from "./components/Footer";
-import DropdownMenu from "./components/DropdownMenu";
 import ShoesData from "./components/ShoesData";
 import BagsData from "./components/BagsData";
 import Filter from "./components/Filter";
-import { SortingProvider } from "./context/DataContext";
+import { FilteringProvider } from "./context/FilterContext";
 
 function App() {
     return (
         <Container>
             <NavigationBar />
-            <Row>
-                <Col className="mt-5" md={2}>
-                    <Filter />
-                </Col>
-                <Col md={10}>
-                    <Routes>
-                        <Route path="/shoes" element={<ShoesData />} />
-                        <Route path="/bags" element={<BagsData />} />
-                    </Routes>
-                </Col>
-            </Row>
+            <FilteringProvider>
+                <Row>
+                    <Col className="mt-5" md={2}>
+                        <Filter />
+                    </Col>
+                    <Col md={10}>
+                        <Routes>
+                            <Route path="/shoes" element={<ShoesData />} />
+                            <Route path="/bags" element={<BagsData />} />
+                        </Routes>
+                    </Col>
+                </Row>
+            </FilteringProvider>
             <Footer />
         </Container>
     );
