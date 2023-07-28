77cbad9f19439397b2583476305bfcd63b3a58e1
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-28 10:27:08 +0300

    Refactor & pass props to dropdown component

--- client/src/components/ShoesData.jsx
+++ client/src/components/ShoesData.jsx
@@ -4,13 +4,15 @@ import data from "../data/data.json";
 
 import utilsStyle from "../styles/utils.module.css";
 import ShoesList from "./ShoesList";
+import DropdownMenu from "./DropdownMenu";
 
 const productsPerRow = 10;
 
 function ShoesData() {
     const [products, setProducts] = useState([]);
     const [dataLoaded, setDataLoaded] = useState(false);
-    const [next, setNext] = useState(productsPerRow)
+    const [next, setNext] = useState(productsPerRow);
+    const [sortedProducts, setSortedProducts] = useState([]);
 
     useEffect(() => {
         const productsArray = data.shoes;
@@ -18,14 +20,20 @@ function ShoesData() {
         setDataLoaded(true);
     }, []);
 
-   
     const handleMoreProducts = () => {
-        setNext(next + productsPerRow)
-    }
+        setNext(next + productsPerRow);
+    };
+
+    const handleSort = (sortedData) => {
+        setSortedProducts(sortedData);
+    };
+
+    const productsToDisplay =
+        sortedProducts.length > 0 ? sortedProducts : products;
 
     const productsGrid = (
         <Row xs={1} md={2} xl={3} xxl={4} className="g-4 mt-3 mb-4">
-            {products?.slice(0, next).map((product) => (
+            {productsToDisplay?.slice(0, next).map((product) => (
                 <Col key={product.id}>
                     <ShoesList product={product} />
                 </Col>
@@ -35,17 +43,30 @@ function ShoesData() {
 
     const shoesList = (
         <>
-            {products.length > 0 ? productsGrid : <h4>No products to show</h4>}
+            {productsToDisplay.length > 0 ? (
+                productsGrid
+            ) : (
+                <h4>No products to show</h4>
+            )}
             <div
                 className={`${utilsStyle.blockCenter} ${utilsStyle.flexCenter}`}
             >
-                <Button onClick={handleMoreProducts} variant="outline-dark" className="mt-3 mb-5">
+                <Button
+                    onClick={handleMoreProducts}
+                    variant="outline-dark"
+                    className="mt-3 mb-5"
+                >
                     Load More
                 </Button>
             </div>
         </>
     );
-    return <>{shoesList}</>;
+    return (
+        <>
+            <DropdownMenu data={products} onSort={handleSort} />
+            {shoesList}
+        </>
+    );
 }
 
 export default ShoesData;
