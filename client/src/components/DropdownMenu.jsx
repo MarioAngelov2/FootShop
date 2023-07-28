413cbd60e036aefa6cb218cc37c608ab716d2993
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-28 10:26:27 +0300

    Added sorting algorithm

--- client/src/components/DropdownMenu.jsx
+++ client/src/components/DropdownMenu.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 import { Dropdown } from "react-bootstrap";
+import utilStyle from "../styles/utils.module.css";
 
-function DropdownMenu() {
+function DropdownMenu({ data, onSort }) {
+    const handleSortChange = (eventKey) => {
+        if (eventKey === "alphabeticalAZ") {
+            let sortedData = [...data].sort((a, b) =>
+                a.title.localeCompare(b.title)
+            );
+            onSort(sortedData);
+        } else if (eventKey === "alphabeticalZA") {
+            let sortedData = [...data].sort((a, b) =>
+                b.title.localeCompare(a.title)
+            );
+            onSort(sortedData);
+        } else if (eventKey === "lowestPrice") {
+            let sortedData = [...data].sort((a, b) => a.price - b.price);
+            onSort(sortedData);
+        } else if (eventKey === "highestPrice") {
+            let sortedData = [...data].sort((a, b) => b.price - a.price);
+            onSort(sortedData);
+        }
+    };
+
     return (
-        <Dropdown>
+        <Dropdown onSelect={handleSortChange}>
             <Dropdown.Toggle variant="outline" id="dropdown-basic">
-                Order by
+                Sort by
             </Dropdown.Toggle>
-            <Dropdown.Menu>
-                <Dropdown.Item>Alphabetical a-z</Dropdown.Item>
-                <Dropdown.Item>Alphabetical z-a</Dropdown.Item>
-                <Dropdown.Item>Price ascending</Dropdown.Item>
-                <Dropdown.Item>Price descending</Dropdown.Item>
+            <Dropdown.Menu className={`${utilStyle.fontSize}`}>
+                <Dropdown.Item className="mb-2 mt-1" eventKey="alphabeticalAZ">
+                    Alphabetical A-Z
+                </Dropdown.Item>
+                <Dropdown.Item className="mb-2" eventKey="alphabeticalZA">
+                    Alphabetical Z-A
+                </Dropdown.Item>
+                <Dropdown.Item className="mb-2" eventKey="lowestPrice">
+                    Lowest price
+                </Dropdown.Item>
+                <Dropdown.Item className="mb-2" eventKey="highestPrice">
+                    Highest price
+                </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
