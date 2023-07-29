5cd4162828dc1ac4e406395f4e1a105555da9fac
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-29 19:42:33 +0300

    Add new color

--- client/src/context/FilterContext.jsx
+++ client/src/context/FilterContext.jsx
@@ -19,6 +19,7 @@ export const FilteringProvider = ({ children }) => {
             brown: false,
             black: false,
             olive: false,
+            blue: false,
         },
     });
 
