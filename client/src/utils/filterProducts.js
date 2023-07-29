431f08a58c8a5e3372995b25176dcb88a2801079
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-29 10:32:52 +0300

    Update filtering method

--- client/src/utils/filterProducts.js
+++ client/src/utils/filterProducts.js
@@ -11,17 +11,35 @@ export function filterProducts(products, filteredData) {
         } else if (filteredData.selectPriceRange.lowerMidRange) {
             return (
                 parseInt(product.price) >= 50 && parseInt(product.price) <= 100
-            )
+            );
         } else if (filteredData.selectPriceRange.midHighRange) {
             return (
                 parseInt(product.price) >= 150 && parseInt(product.price) <= 250
-            ) 
+            );
         } else if (filteredData.selectPriceRange.all) {
-            return (
-                parseInt(product.price) > 250 
-            ) 
+            return parseInt(product.price) > 250;
         }
 
-        return false
+        if (!filteredData.selectColor) {
+            return true;
+        }
+
+        if (filteredData.selectColor.red) {
+            return product.color === "red";
+        } else if (filteredData.selectColor.white) {
+            return product.color === "white";
+        } else if (filteredData.selectColor.pink) {
+            return product.color === "pink";
+        } else if (filteredData.selectColor.yellow) {
+            return product.color === "yellow";
+        } else if (filteredData.selectColor.brown) {
+            return product.color === "brown";
+        } else if (filteredData.selectColor.black) {
+            return product.color === "black";
+        } else if (filteredData.selectColor.olive) {
+            return product.color === "olive";
+        }
+
+        return false;
     });
 }
